21dd5123bcec33de08c94d21c27b56ac2b8b596e
ruuuu <rufinka_91@mail.ru>
2025-04-06 11:05:57 +0300

    add slider

--- src/main.js
+++ src/main.js
@@ -1,2 +1,32 @@
 import 'normalize.css';
 import './style.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper/modules';
+
+import 'swiper/css/navigation';
+import 'swiper/css';
+
+
+
+
+
+
+
+
+const swiper = new Swiper(".slider-thumbnails", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  modules: [Navigation, Pagination],
+});
+const  swiper2 = new Swiper(".product__slider", { // 
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".product__slider-arrow--next",
+    prevEl: ".product__slider-arrow--prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  modules: [ Navigation, Thumbs]
+});
